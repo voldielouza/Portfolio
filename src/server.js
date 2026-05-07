8c6d553e3db8ebc214681608c84f570bcff33988
Voldie <kakulululouza@gmail.com>
2026-05-07 17:43:33 -0400

    5/6/26 Changes

--- src/server.js
+++ src/server.js
@@ -1,1 +1,38 @@
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const app= express();
+const PORT = 3000;
+const host = 'localhost';
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(morgan('dev'));
+app.use(express.static('src/public'));
+
+app.get('/', (req, res) => {
+    // Eventually, this data might come from a database or a JSON file
+    const projects = [
+        { name: "My Portfolio", description: "Node.js app" },
+        { name: "Weather App", description: "API project" }
+    ];
+
+    res.render('index', { 
+        title: 'Home', 
+        projects: projects // Passing the data to the view
+    });
+});
+if(process.env.NODE_ENV !== 'test') {
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
+}
+
+export default app;
